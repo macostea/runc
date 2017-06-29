28f85252be0a115c99ef704e085c9d4f5414e723
Hongjiang Zhang <honzhan@microsoft.com>
2017-06-29 17:15:39 +0800

    implement 'kill'

--- main_freebsd.go
+++ main_freebsd.go
@@ -6,7 +6,7 @@ var (
 	checkpointCommand cli.Command
 	eventsCommand     cli.Command
 	restoreCommand    cli.Command
-	killCommand       cli.Command
+	//killCommand       cli.Command
 	initCommand       cli.Command
 	pauseCommand      cli.Command
 	resumeCommand     cli.Command
